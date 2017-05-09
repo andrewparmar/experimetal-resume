d4b6b61109cc2a8688274da948635a6a78e6ac91
Andrew Parmar <andrew.parmar@gmail.com>
2017-05-08 22:31:15 -0700

    First commit

---
+++ js/resumeBuilder.js
@@ -0,0 +1,22 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+
+ $("#main").append("Andrew Parmar");
+
+ var aweomeThoughts = "I am andrew, and I am awesome";
+
+ console.log(aweomeThoughts);
+
+
+var funThoughts = aweomeThoughts.replace("awesome","FuN");
+
+ $("#main").append(funThoughts);
+
+
+
+ var formattedName = HTMLheaderName.replace('%data%',"Andrew Parmar");
+ var formattedRole = HTMLheaderRole.replace('%data%',"Engineer");	
+
+  $("#header").append(formattedName);
+  $("#header").append(formattedRole);
