7e1a10f5c809e7a026e494fe93d88f308b8bbf1d
Andrew Parmar <andrew.parmar@gmail.com>
2017-08-26 16:03:31 -1000

    Changed homepage to portfolio headshot view, added site maintenance page, and resume route

--- portfolio.py
+++ portfolio.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 @app.route('/')
-def resume():
+def home():
 	# return "Hello World"
+	return render_template('portfolio_home.html')
+
+@app.route('/resume')
+def resume():
 	return render_template('resume2.html')
+
+@app.route('/projects')
+def projects():
+	return render_template('site_maintenance.html')
+
 
 if __name__ == "__main__":
 	app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
